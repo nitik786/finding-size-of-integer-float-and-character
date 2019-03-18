22b8ece7a22095599f28f0a0983b16ee36f8be8c
Nitik Tiwari <48062186+nitik786@users.noreply.github.com>
2019-03-18 21:48:08 +0545

    Add files via upload

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a;
+	char c;
+	float f;
+	printf("the size of int is %d\n",sizeof(int));
+	printf("the size of char is %d\n",sizeof(char));
+	printf("the size of float is %d\n",sizeof(float));
+	return 0;
+}
